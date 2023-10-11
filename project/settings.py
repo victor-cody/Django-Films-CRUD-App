a61329bd172934f1acd3e942116ebd992d49fbe8
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2023-10-12 00:23:51 +0100

    updates to project configurations

--- project/settings.py
+++ project/settings.py
@@ -35,7 +35,7 @@ DEBUG = env('DEBUG')  # <-- Updated!``
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://*.herokuapp.com', 'https://*.herokuapp.com']
 
 # Application definition
 
@@ -94,8 +94,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # }
  
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + os.path.join('db.sqlite3'))    
+    'default': dj_database_url.config(default='sqlite:///' + os.path.join('db.sqlite3'), conn_max_age=600, conn_health_checks=True)    
 }
+
+# Check if the app is running on Heroku (look for DATABASE_URL in environment variables)
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600, conn_health_checks=True)}
+# else:
+#     # If not on Heroku, use the default SQLite database
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
 # Password validation
